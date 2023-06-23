e3edaa8beb08252b2051e296f6407f28366c6a95
clnzbqll <clnzbqll@tiedada.com>
2023-06-23 23:02:38 +0800

    # admin模块

---
+++ yugi-admin/src/main/java/com/example/yugi/YugiAdminApplication.java
@@ -0,0 +1,14 @@
+package com.example.yugi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YugiAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YugiAdminApplication.class, args);
+        System.out.println("阿姨，一份儿童套餐！！！！");
+    }
+
+}
---
+++ yugi-admin/src/test/java/com/example/yugi/YugiAdminApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.yugi;
+
+import com.example.yugi.sys.ILoginService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+@SpringBootTest(classes = {YugiAdminApplication.class})//测试类需要指定启动类
+class YugiAdminApplicationTests {
+    @Resource
+    private ILoginService loginService;
+
+
+    @Test
+    void contextLoads() {
+        loginService.login();
+        System.out.println("测试成功");
+    }
+
+}
